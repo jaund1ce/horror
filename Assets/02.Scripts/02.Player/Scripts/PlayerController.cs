2c25cb0b69129c36bc06453d5e20828ea2a9e957
jaund1ce <66049637+jaund1ce@users.noreply.github.com>
2024-12-05 19:55:37 +0900

    [Feature] add fisheye effect fail

--- Assets/02.Scripts/02.Player/Scripts/PlayerController.cs
+++ Assets/02.Scripts/02.Player/Scripts/PlayerController.cs
@@ -10,49 +10,51 @@ using UnityEngine.Windows;
 
 public class PlayerController : MonoBehaviour
 {
-    public PlayerInputs playerInputs {  get; private set; }//inputsystem generate c# script로 생성된 스크립트
-    public PlayerInputs.PlayerActions playerActions { get; private set; }   //미리 정의한 행동들 move, look,... 등
+    public PlayerInputs playerInputs { get; private set; } // inputsystem generate c# script로 생성된 스크립트
+    public PlayerInputs.PlayerActions playerActions { get; private set; } // 미리 정의한 행동들 move, look,... 등
 
-    [SerializeField] private CinemachineVirtualCamera playercamera;
+    [SerializeField] private CinemachineVirtualCamera playerCamera;
+    [SerializeField] private float rotateSensitivity = 0.02f;
+    [SerializeField] private float verticalClampMin = -45f;
+    [SerializeField] private float verticalClampMax = 45f;
+
     private CinemachinePOV pov;
-    private float rotateSencitivity = 0.02f;
 
     private void Awake()
     {
         playerInputs = new PlayerInputs();
-        playerActions = playerInputs.Player;//inputsystem에 선언했던 Actionmap 중에 하나를 선택
-        pov = playercamera.GetCinemachineComponent<CinemachinePOV>();
-        playerInputs.Enable();
+        playerActions = playerInputs.Player; // inputsystem에 선언했던 Actionmap 중에 하나를 선택
+        pov = playerCamera.GetCinemachineComponent<CinemachinePOV>();
     }
 
     private void OnEnable()
     {
         playerInputs.Enable();
-        playerActions.Look.started += RotateAll;
+        playerActions.Look.performed += RotateAll; // performed로 변경
     }
 
     private void OnDisable()
     {
         playerInputs.Disable();
-        playerActions.Look.started -= RotateAll;
+        playerActions.Look.performed -= RotateAll;
     }
 
-    private void RotateAll(InputAction.CallbackContext context)//cinemachine의 aim방식에 따라서 회전시키는 방법은 다르다.
+    private void RotateAll(InputAction.CallbackContext context) // cinemachine의 aim방식에 따라서 회전시키는 방법은 다르다.
     {
         Vector2 delta = context.ReadValue<Vector2>();
 
         if (delta != Vector2.zero)
         {
-            float rotatex = Mathf.Clamp(delta.y, -60f, 60f);
-            float rotatey = Mathf.Clamp(delta.x, -90f, 90f);
+            float rotateX = delta.y * rotateSensitivity * Time.deltaTime;
+            float rotateY = delta.x * rotateSensitivity * Time.deltaTime;
 
-            transform.rotation *= Quaternion.Euler(0f, rotatey * rotateSencitivity, 0f);
+            transform.rotation *= Quaternion.Euler(0f, rotateY, 0f);
 
-            if (pov == null) return;
-
-            pov.m_VerticalAxis.Value -= rotatex * rotateSencitivity;
-            pov.m_VerticalAxis.Value = Mathf.Clamp(pov.m_VerticalAxis.Value, -45f, 45f);//나중에 외부에서 변경 후 제거
+            if (pov != null)
+            {
+                pov.m_VerticalAxis.Value -= rotateX;
+                pov.m_VerticalAxis.Value = Mathf.Clamp(pov.m_VerticalAxis.Value, verticalClampMin, verticalClampMax);
+            }
         }
-
     }
 }
