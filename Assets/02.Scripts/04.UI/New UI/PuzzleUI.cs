26510bd587cfb4a2ef257cdef6f585454241e16c
PRODOINGER <wjddydghk668@naver.com>
2024-12-07 18:18:15 +0900

    [Fix]UI

---
+++ Assets/02.Scripts/04.UI/New UI/PuzzleUI.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PuzzleUI : BaseUI
+{
+    public void InteractWithPuzzle()
+    {
+        OpenUI();
+    }
+
+    public override void OpenUI()
+    {
+        base.OpenUI();
+    }
+
+    public override void CloseUI()
+    {
+        base.CloseUI();
+    }
+}
