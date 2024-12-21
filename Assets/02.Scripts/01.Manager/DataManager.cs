f38c6983ab3ffaad7af6dd1a1bba3acabde22f03
PRODOINGER <wjddydghk668@naver.com>
2024-12-21 14:12:39 +0900

    [Adding] Manager

--- Assets/02.Scripts/01.Manager/DataManager.cs
+++ Assets/02.Scripts/01.Manager/DataManager.cs
@@ -48,6 +48,6 @@ public class DataManager : mainSingleton<DataManager>
     private void LoadData()
     {
         var json = ResourceManager.Instance.LoadAsset<TextAsset>("ItemSO", eAssetType.Data); // Resources/Data/ItemSo파일을 자산으로 활용하기 위해 assetPool에 등록 후 지역변수 json에 TextAsset자료형으로 할당
-        itemSO = JsonUtility.FromJson<ItemData>(json.text); //
+        itemSO = JsonUtility.FromJson<ItemData>(json.text); //TextAsset에서 JSON 문자열 데이터 추출하여 JSON 데이터를 ItemData로 변환하여 itemSO에 저장
     }
 }
--- Assets/02.Scripts/10.Scene/EndScene.cs
+++ Assets/02.Scripts/10.Scene/EndScene.cs
@@ -7,6 +7,8 @@ public class EndScene : SceneBase
     // Start is called before the first frame update
     void Start()
     {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
         UIManager.Instance.Show<EndUI>();
     }
 
