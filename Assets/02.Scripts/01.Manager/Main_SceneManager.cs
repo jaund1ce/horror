2284d0e2745616e4cc52ec1744e4edffd899c42f
PRODOINGER <wjddydghk668@naver.com>
2025-01-14 20:56:04 +0900

    [Fix] Refactoring

--- Assets/02.Scripts/01.Manager/Main_SceneManager.cs
+++ Assets/02.Scripts/01.Manager/Main_SceneManager.cs
@@ -1,7 +1,6 @@
 using UnityEditor;
 using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.SceneManagement;
